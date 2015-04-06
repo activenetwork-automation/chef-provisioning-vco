cf85f8d7ef96bfd29f174298798975e088357bd3
Gregory Ruiz-Ade <gkra@unnerving.org>
2015-04-06 15:09:32 -0700

    Initial Commit

---
+++ lib/chef/provisioning/vco.rb
@@ -0,0 +1,9 @@
+require "chef/provisioning/vco/version"
+
+module Chef
+  module Provisioning
+    module Vco
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/chef/provisioning/vco/version.rb
@@ -0,0 +1,7 @@
+module Chef
+  module Provisioning
+    module Vco
+      VERSION = "0.1.0"
+    end
+  end
+end
