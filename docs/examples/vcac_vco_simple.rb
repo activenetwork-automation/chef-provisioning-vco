2be2ca43c39f9db41f139ca4a5161daf74f7d73e
Gregory Ruiz-Ade <gkra@unnerving.org>
2015-04-14 14:11:47 -0700

    simplify example

--- docs/examples/vcac_vco_simple.rb
+++ docs/examples/vcac_vco_simple.rb
@@ -9,33 +9,20 @@ with_driver 'vco:atom-active:ref2', :vco_options => {
 
 components = %q(webserver apiserver dbserver)
 
-case chef_environment
-when 'int', 'qa', 'stage'
-  cluster_size = 2
-when 'prod'
-  cluster_size = 4
-else
-  cluster_size = 1
-end
-
 machine_batch do
   components.each do |component|
     component_role = "#{vco_options[:business_unit]}-#{component}"
     1.upto(cluster_size).do |i| do
       machine "#{chef_environment}-#{component}-#{i}" do
-        machine_options bootstrap_options: {
-          reservation_policy: 'nonprod',
-          environment:        chef_environment,
-          on_behalf_of:       'svc_vco@example.com',
-          location:           'uswest',
-          component:          component,
-          cpu:                1,
-          ram:                512,
-          image:              nil
-        }
-        role 'loc_uswest'
-        role 'active-base'
-        role component_role
+        machine_options :reservation_policy => 'nonprod',
+                        :on_behalf_of => 'svc_vco@example.com',
+                        :location => 'uswest',
+                        :environment => chef_environment,
+                        :component => component,
+                        :cpu => compute[component][:cpu],
+                        :ram => compute[component][:ram],
+                        :image => compute[component][:image]
+        run_list ['role[active-base]', "role[loc_#{location}]", component_role]
       end
     end
   end
