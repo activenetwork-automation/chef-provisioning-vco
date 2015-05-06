1ba6b930016b85720d9b360510803f0655ecde95
Gregory Ruiz-Ade <gkra@unnerving.org>
2015-05-05 17:57:33 -0700

    Update example

--- docs/examples/vcac_vco_simple.rb
+++ docs/examples/vcac_vco_simple.rb
@@ -2,59 +2,40 @@
 
 require 'chef/provisioning/vco_driver'
 
-# Some variables that control where and what we are building
-on_behalf_of            = 'svc.vco@example.com'
-vcac_tenant             = 'vcac_tenant'
-business_unit           = 'ref2'
-reservation_policy      = 'nonprod'
-site                    = 'uswest'
-target_chef_environment = 'dev1'
-component               = 'webserver'
-instance_id             = '03'
-cpu_count               = 2
-ram_mb                  = 2048
-vm_template             = 'centos-6.6-x86_64-20150325-1'
+with_driver("vco:atom-active:ref2", {
+  vco_options: {
+    url:           'https://vco.example.com:8281/',
+    verify_ssl:    false,
+    username:      'EXAMPLE\user',
+    password:      'password',
+    max_wait:      900,
+    wait_interval: 15
+  }
+})
 
-# Construct the node name
-node_name = "#{target_chef_environment}-#{component}-#{instance_id}"
+with_machine_options({
+  ssh_username:      'knife',
+  sudo:              true,
+  is_windows:        false,
+  image:             'centos-6.6-x86_64-20150325-1',
+  bootstrap_options: {
+    key_path:           File.join(ENV['HOME'], '.ssh', 'knife_rsa'),
+    reservation_policy: 'nonprod',
+    on_behalf_of:       'svc.vco@dev.atom.int',
+    environment:        'lab1',
+    component:          'webserver',
+    cpu:                1,
+    ram:                1024
+  }
+})
 
-# Configure the driver
-with_driver "vco:#{vcac_tenant}:#{business_unit}", :vco_options => {
-  url:           'https://vcoserver.example.com:8281/',
-  verify_ssl:    false,
-  username:      'joeuser',
-  password:      'passwords_suck',
-  max_wait:      900,
-  wait_interval: 15
-}
+with_chef_server('https://chef.example.com/organizations/myorg')
 
 # Build the machine
-machine node_name do
-  # Chef Server to join
-  chef_server "https://chef.example.com/organizations/#{business_unit}"
-
-  # The chef environment the machine will live in
-  chef_environment target_chef_environment
-
-  # The bootstrap runlist
+machine 'webserver01' do
+  role 'site'
   role 'base'
-  role "#{site}"
-  role "#{business_unit}-#{component}"
+  role 'webserver'
 
-  # Options that we need to pass through to the driver;
-  # These are very specific to the vCenter Orchestrator workflow we're
-  # using to handle the machine allocation.
-  machine_options ({
-    :reservation_policy => reservation_policy,
-    :on_behalf_of       => on_behalf_of,
-    :location           => site,
-    :environment        => target_chef_environment,
-    :component          => component,
-    :cpu                => cpu_count,
-    :ram                => ram_mb,
-    :image              => vm_template,
-    :ssh_username       => 'knife',
-    :sudo               => true,
-    :is_windows         => false
-  })
+  chef_environment 'lab1'
 end
